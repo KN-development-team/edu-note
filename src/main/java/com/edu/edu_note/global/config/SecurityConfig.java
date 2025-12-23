2d1fb7db7a788b4f8bf544df09dabc573ce9752a
eunkyoung <eunkyoung4160@gmail.com>
2025-12-24 01:24:49 +0900

    Feat : 내용 페이지 UI/UX 추가

--- src/main/java/com/edu/edu_note/global/config/SecurityConfig.java
+++ src/main/java/com/edu/edu_note/global/config/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
                                 "/",
                                 "/index.html",
                                 "/dashboard.html",
+                                "/content.html",
                                 "/quiz.html",
 //                                "/favicon.ico",
                                 "/static/**",
